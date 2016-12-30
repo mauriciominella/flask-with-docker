8531b41b62a9b292b53d59559c2381cac52972af
Mauricio Minella <mauriciominella@gmail.com>
2016-12-29 22:17:50 -0200

    Adding mongodb connection to the app

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+import os
 from flask import Flask
+from pymongo import MongoClient
+
 app = Flask(__name__)
+
+client = MongoClient(
+        os.environ['DB_PORT_27017_TCP_ADDR'],
+        int(os.environ['MONGO_DB_PORT']))
+db = client.testdb
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized'
+    connections_dict = db.command("serverStatus")["connections"]
+    return connections_dict
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
